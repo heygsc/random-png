deb347b1195d640e00032d457a4140f6ef65e5bc
heygsc <1596920983@qq.com>
2026-05-08 10:54:40 +0800

    Resize random image grid cells

--- generate-random-image.js
+++ generate-random-image.js
@@ -1,9 +1,12 @@
 import { createWriteStream } from 'node:fs';
 import { PNG } from 'pngjs';
 
-// Image size. Change these values if needed.
-const WIDTH = 3;
-const HEIGHT = 3;
+// Grid size and rendered cell size. Change these values if needed.
+const GRID_COLUMNS = 3;
+const GRID_ROWS = 3;
+const CELL_SIZE = 80;
+const WIDTH = GRID_COLUMNS * CELL_SIZE;
+const HEIGHT = GRID_ROWS * CELL_SIZE;
 const OUTPUT_FILE = 'random.png';
 
 function randomRGB() {
@@ -17,14 +20,21 @@ function randomRGB() {
 async function generateRandomImage() {
   const image = new PNG({ width: WIDTH, height: HEIGHT });
 
-  for (let x = 0; x < WIDTH; x += 1) {
-    for (let y = 0; y < HEIGHT; y += 1) {
+  for (let gridX = 0; gridX < GRID_COLUMNS; gridX += 1) {
+    for (let gridY = 0; gridY < GRID_ROWS; gridY += 1) {
       const { r, g, b } = randomRGB();
-      const index = (WIDTH * y + x) << 2;
-      image.data[index] = r;
-      image.data[index + 1] = g;
-      image.data[index + 2] = b;
-      image.data[index + 3] = 255;
+
+      for (let cellX = 0; cellX < CELL_SIZE; cellX += 1) {
+        for (let cellY = 0; cellY < CELL_SIZE; cellY += 1) {
+          const x = gridX * CELL_SIZE + cellX;
+          const y = gridY * CELL_SIZE + cellY;
+          const index = (WIDTH * y + x) << 2;
+          image.data[index] = r;
+          image.data[index + 1] = g;
+          image.data[index + 2] = b;
+          image.data[index + 3] = 255;
+        }
+      }
     }
   }
 
